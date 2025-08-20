ed9af8655a77ab18bc4f2b1b34ec8699125b73fc
LuanComputacao <eng.luanroger@gmail.com>
2025-08-20 16:47:45 -0300

    fix: instalation php 7.4.33

--- asdf_vm/_php.sh
+++ asdf_vm/_php.sh
@@ -4,7 +4,11 @@ sudo apt-get update
 sudo apt-get install -y autoconf bison build-essential curl gettext git libgd-dev libcurl4-openssl-dev libedit-dev libicu-dev libjpeg-dev libmysqlclient-dev libonig-dev libpng-dev libpq-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libzip-dev openssl pkg-config re2c zlib1g-dev
 asdf plugin add php
 
-asdf install php 7.3.31
+CPPFLAGS="-Wno-deprecated-declarations -DRSA_SSLV23_PADDING=RSA_PKCS1_PADDING" \
+  CFLAGS="-Wno-deprecated-declarations" \
+  PHP_CONFIGURE_OPTIONS="--with-openssl" \
+  asdf install php 7.4.33
+
 asdf install php 8.0.11
 
 asdf global php 8.0.11
