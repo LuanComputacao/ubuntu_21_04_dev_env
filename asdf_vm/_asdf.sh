f3a8b04b0bbac67b5930679558cf91f130fab0d4
LuanComputacao <eng.luanroger@gmail.com>
2025-08-23 00:48:24 -0300

    feat: update to 2025

--- asdf_vm/_asdf.sh
+++ asdf_vm/_asdf.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 
-echo '. $HOME/.asdf/asdf.sh' >>~/.zshrc
+curl https://github.com/asdf-vm/asdf/releases/download/v0.18.0/asdf-v0.18.0-linux-amd64.tar.gz
+
+tar -zxvf asdf-v0.18.0-linux-amd64.tar.gz
+
+echo 'export PATH="$PATH:/home/luancomputacao/.asdf"' >> .zshrc
+
+# Add shims directory to path
+echo 'export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"' >> .zshrc
+
+# Set up shell completions
+mkdir -p "${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
+asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
+
+# Append completions to fpath
+fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
+# initialise completions with ZSH's compinit
+autoload -Uz compinit && compinit
+
 
 
 
